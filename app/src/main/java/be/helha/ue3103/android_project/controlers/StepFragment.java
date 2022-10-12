92ed0f1fd3f6451110c414f603f1490c85ec5cd4
RobinMory <la197222@student.helha.be>
2022-10-13 00:19:31 +0200

    Ajout du fragment STEP

--- app/src/main/java/be/helha/ue3103/android_project/controlers/StepFragment.java
+++ app/src/main/java/be/helha/ue3103/android_project/controlers/StepFragment.java
@@ -1,7 +1,11 @@
 package be.helha.ue3103.android_project.controlers;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,9 +18,17 @@ public class StepFragment extends Fragment {
 
     protected Step mStep;
     private Spinner mPointsSpinner;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mStep = new Step();
+
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.step_fragment,container,false);
+        return v;
     }
 }
--- app/src/main/java/be/helha/ue3103/android_project/controlers/StepListActivity.java
+++ app/src/main/java/be/helha/ue3103/android_project/controlers/StepListActivity.java
@@ -1,10 +1,13 @@
 package be.helha.ue3103.android_project.controlers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -22,6 +25,9 @@ public class StepListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steplist);
+
+
+
         mSaveButton = findViewById(R.id.save_project);
         mAddStepButton = findViewById(R.id.floatingActionButton1);
         mProjectName = findViewById(R.id.project_title);
@@ -32,6 +38,15 @@ public class StepListActivity extends AppCompatActivity {
     private void setButtonListener() {
         mAddStepButton.setOnClickListener(event -> {
             //Ouvrir un fragment en envoyant une étape en paramètre
+            Toast.makeText(this, "Fragment Open from StepList", Toast.LENGTH_SHORT).show();
+            FragmentManager fm = getSupportFragmentManager();
+            Fragment fragment = fm.findFragmentById(R.id.step_list_container);
+            //if (fragment == null) {
+                fragment = new StepFragment();
+                fm.beginTransaction()
+                        .add(R.id.step_list_container, fragment)
+                        .commit();
+            //}
         });
     }
 }
