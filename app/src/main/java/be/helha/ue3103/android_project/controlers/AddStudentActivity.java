68e81b2dd5d00012db8f791a913c15f611b65993
RobinMory <la197222@student.helha.be>
2022-10-18 10:27:01 +0200

    Ajout des nouvelles vues

---
+++ app/src/main/java/be/helha/ue3103/android_project/controlers/AddStudentActivity.java
@@ -0,0 +1,16 @@
+package be.helha.ue3103.android_project.controlers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import be.helha.ue3103.android_project.R;
+
+public class AddStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+    }
+}
---
+++ app/src/main/java/be/helha/ue3103/android_project/controlers/StudentListActivity.java
@@ -0,0 +1,16 @@
+package be.helha.ue3103.android_project.controlers;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import be.helha.ue3103.android_project.R;
+
+public class StudentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_list);
+    }
+}
